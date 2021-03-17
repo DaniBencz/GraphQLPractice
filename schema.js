bb8f452e0680cc44d559432b19bbf775306ed03d
Daniel <daniel.bencz@gmail.com>
2021-03-17 10:10:01 +0100

    basic graphql setup

---
+++ schema.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import { buildSchema } from 'graphql';
+
+const schema = buildSchema(`
+    type Query{
+        hello: String
+    }
+`)
+
+export default schema
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+'use strict';
+
 import express from 'express';
+import schema from './schema.js';
+import { graphqlHTTP } from 'express-graphql';
 
 const app = express();
-const port = process.env.PORT || 4040
+const port = process.env.PORT || 4040;
 
 app.get('/', (req, res) => {
     res.send('Hello!');
-})
+});
+
+const root = { hello: () => 'Hi from QraphQL' };
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+}))
 
 app.listen(port, () => console.log(`server running on port ${port}`));
