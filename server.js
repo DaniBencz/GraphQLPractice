08d0ea5afe374db42438eafb387ece9e63c4465d
Daniel Bencz <daniel.bencz@gmail.com>
2021-04-01 11:02:56 +0200

    terminus and helmet

--- server.js
+++ server.js
@@ -1,21 +1,51 @@
 'use strict';
 
-(async () => {
-    const { default: express } = await import('express');
-    const { graphqlHTTP } = await import('express-graphql');
-    const { schema } = await import('./schema.js');
+import http from 'http';
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { createTerminus } from '@godaddy/terminus';
+import helmet from 'helmet';
 
-    const app = express();
-    const port = process.env.PORT || 4040;
+import { schema } from './schema.js';
+import pool from './dbConnection.js';
 
-    app.get('/', (req, res) => {
-        res.send('Hello!');
-    });
+const app = express();
+const server = http.createServer(app);
+const port = process.env.PORT || 4040;
 
-    app.use('/graphql', graphqlHTTP({
-        schema,
-        graphiql: true,
-    }));
+// unfortunately, helmet clashes with graphql...
+// app.use(helmet());
 
-    app.listen(port, () => console.log(`server running on port ${port}`));
-})();
+app.get('/', (req, res) => {
+    res.send('Hello!');
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true,
+}));
+
+const onHealthCheck = async () => {
+    // checks if the system is healthy, like the db connection is live
+    // resolves, if health, rejects if not
+};
+
+const onSignal = () => {
+    console.log('\nserver is starting cleanup');
+    return Promise.all([
+        pool.end()
+    ]);
+};
+
+const onShutdown = () => {
+    console.log('cleanup finished, server is shutting down');
+};
+
+createTerminus(server, {
+    signal: 'SIGINT',
+    healthChecks: { '/healthcheck': onHealthCheck },
+    onSignal,
+    onShutdown
+});
+
+server.listen(port, () => console.log(`server running on port ${port}`));
